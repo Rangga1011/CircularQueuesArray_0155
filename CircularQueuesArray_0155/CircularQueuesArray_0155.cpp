a376b94b5e1e376d069a79be30c17e056ad4ad83
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-05-30 11:33:06 +0700

    Menambahkan void remove  jika element dihapus berada di pusisi terakhir

--- CircularQueuesArray_0155/CircularQueuesArray_0155.cpp
+++ CircularQueuesArray_0155/CircularQueuesArray_0155.cpp
@@ -53,4 +53,14 @@ public:
 			FRONT = -1;
 			REAR = -1;
 		}
-		
+		else {
+			//jika elemen dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+
+		}
+	}
+
+	
