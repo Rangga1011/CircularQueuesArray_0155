7a6273b6fd089fa317724cad64aeae6c8ac16e1b
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-05-30 11:28:25 +0700

    Menambahkan jika rear berada di posisi terakhir array, kembali ke awal array

--- CircularQueuesArray_0155/CircularQueuesArray_0155.cpp
+++ CircularQueuesArray_0155/CircularQueuesArray_0155.cpp
@@ -2,75 +2,42 @@
 using namespace std;
 
 class Queues {
-    int FRONT, REAR, max = 5;
-    int queue_array[5];   
-};
+	int FRONT, REAR, max = 5;
+	int queue_array[5];
+
 
 public:
-    Queues() {
-        FRONT = -1;
-        REAR = -1;
-    }
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
 
-    void insert() {
-        int num;
-        cout << "Enter a number: ";
-        cin >> num;
-        cout << endl;
+	void insert() {
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << endl;
 
-    }
+		//cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
 
-    // cek apakah antrian penuh
-    if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
-        cout << "\nQueue overflow\n";
-        return 0;
-    }
+		//cek apakah atrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//jika rear berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
 
-    // cek apakah antrian kosong
-    if (FRONT == -1) {
-        FRONT = 0;
-        REAR = 0;
-    }
+		queue_array[REAR] = num;
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	
