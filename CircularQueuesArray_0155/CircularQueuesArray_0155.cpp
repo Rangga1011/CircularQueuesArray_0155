00cc32ad6c64980b1da4f96152b87bcdecebe67a
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-05-30 11:55:57 +0700

    Menambahkan int main Queues

--- CircularQueuesArray_0155/CircularQueuesArray_0155.cpp
+++ CircularQueuesArray_0155/CircularQueuesArray_0155.cpp
@@ -87,7 +87,7 @@ public:
 			//jika front > rear, iterasi dai front hingga akhir array
 			while (FRONT_Position <= max - 1) {
 				cout << queue_array[FRONT_Position] << " ";
-				FRONT++;
+				FRONT_Position++;
 			}
 
 			FRONT_Position = 0;
@@ -103,4 +103,52 @@ public:
 	}
 };
 
-i
+int main() {
+	Queues q;
+	char ch;
+
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement insert operation" << endl;
+			cout << "2. Implement delete operation" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4): " << endl;
+			cin >> ch;
+			cout << endl;
+
+
+			switch (ch)
+			{
+			case '1': {
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				return 0;
+				break;
+			}
+
+			default: {
+				cout << "Invalid option!!" << endl;
+				break;
+			}
+			}
+
+
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered" << endl;
+		}
+	}
+	return 0;
+}
